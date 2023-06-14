4b2e23b2c3424f45c605fe27b962d9ab00122514
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-13 23:47:08 -0400

    Implementing likes with redis

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -3,4 +3,4 @@ export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionsKey = (sessionId: string) => `session#${sessionId}`;
 export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const usernamesUniqueKey = () => 'usernames:unique';
-
+export const userLikesKey = (userId: string) => `users:likes#${userId}`;
--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,26 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { client } from '$services/redis';
+import { userLikesKey, itemsKey } from '$services/keys';
+
+export const userLikesItem = async (itemId: string, userId: string) => {
+    return client.sIsMember(userLikesKey(userId), itemId);
+};
 
 export const likedItems = async (userId: string) => {};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+    const inserted = await client.sAdd(userLikesKey(userId), itemId);
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+    if (inserted) {
+        return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+    }
+};
+
+export const unlikeItem = async (itemId: string, userId: string) => {
+    const removed = await client.sRem(userLikesKey(userId), itemId);
+
+    if (removed) {
+        return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+    }
+};
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
