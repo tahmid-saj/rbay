10e875808ecb3685c53ea1e230a226d82546de29
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-13 23:54:41 -0400

    Implementing likes with redis

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,11 +1,18 @@
 import { client } from '$services/redis';
 import { userLikesKey, itemsKey } from '$services/keys';
+import { getItems } from './items';
 
 export const userLikesItem = async (itemId: string, userId: string) => {
     return client.sIsMember(userLikesKey(userId), itemId);
 };
 
-export const likedItems = async (userId: string) => {};
+export const likedItems = async (userId: string) => {
+    // Fetch all the item IDs from this user's liked set
+    const ids = await client.sMembers(userLikesKey(userId));
+
+    // Fetch all the item hashes with those ids and return as array
+    return getItems(ids);
+};
 
 export const likeItem = async (itemId: string, userId: string) => {
     const inserted = await client.sAdd(userLikesKey(userId), itemId);
@@ -23,4 +30,8 @@ export const unlikeItem = async (itemId: string, userId: string) => {
     }
 };
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+    const ids = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)]);
+
+    return getItems(ids);
+};
