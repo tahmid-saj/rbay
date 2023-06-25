b82a425da548942740cc94bbeedda7beac4ca3f7
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-25 19:49:27 -0400

    Implementing sign in with sorted set

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -4,3 +4,4 @@ export const sessionsKey = (sessionId: string) => `session#${sessionId}`;
 export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const usernamesUniqueKey = () => 'usernames:unique';
 export const userLikesKey = (userId: string) => `users:likes#${userId}`;
+export const usernamesKey = () => "usernames";
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,9 +1,27 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey, usernamesUniqueKey } from '$services/keys';
+import { usersKey, usernamesUniqueKey, usernamesKey } from '$services/keys';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+    // Use the username argument to look up the persons User ID
+    // with the usernames sorted set
+    const decimalId = await client.zScore(usernamesKey(), username);
+
+    // make sure we actually got an ID from the lookup
+    if (!decimalId) {
+        throw new Error("User does not exist");
+    }
+
+    // Take the ID and convert it back to hex
+    const id = decimalId.toString(16);
+    // Use the ID to lookup the user's hash
+    const user = await client.hGetAll(usersKey(id));
+
+    // Deserialize and return the hash
+    return deserialize(id, user);
+
+};
 
 export const getUserById = async (id: string) => {
     const user = await client.hGetAll(usersKey(id));
@@ -25,6 +43,10 @@ export const createUser = async (attrs: CreateUserAttrs) => {
     // otherwise, continue
     await client.hSet(usersKey(id), serialize(attrs));
     await client.sAdd(usernamesUniqueKey(), attrs.username);
+    await client.zAdd(usernamesKey(), {
+        value: attrs.username,
+        score: parseInt(id, 16)
+    });
 
     return id;
 };
