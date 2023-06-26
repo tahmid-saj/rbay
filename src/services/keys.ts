1eecc1242cc99f3bf66b7e0a1c9743ebd05fc79c
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-25 21:35:27 -0400

    Creating items views

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,7 +1,11 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionsKey = (sessionId: string) => `session#${sessionId}`;
-export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const usernamesUniqueKey = () => 'usernames:unique';
 export const userLikesKey = (userId: string) => `users:likes#${userId}`;
 export const usernamesKey = () => "usernames";
+
+// Items
+export const itemsKey = (itemId: string) => `items#${itemId}`;
+export const itemsByViewsKey = () => "items:views";
+export const itemsByEndingAtKey = () => "items:endingAt";
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -2,7 +2,7 @@ import type { CreateItemAttrs } from '$services/types';
 import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { genId } from '$services/utils';
-import { itemsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey, itemsByEndingAtKey } from '$services/keys';
 import { deserialize } from './deserialize';
 
 export const getItem = async (id: string) => {
@@ -37,6 +37,22 @@ export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
     const serialized = serialize(attrs);
 
     await client.hSet(itemsKey(id), serialized);
+    await client.zAdd(itemsByViewsKey(), {
+        value: id,
+        score: 0
+    });
+
+    await Promise.all([
+        client.hSet(itemsKey(id), serialized),
+        client.zAdd(itemsByViewsKey(), {
+            value: id,
+            score: 0
+        }),
+        client.zAdd(itemsByEndingAtKey(), {
+            value: id,
+            score: attrs.endingAt.toMillis()
+        })
+    ]);
 
     return id;
 };
