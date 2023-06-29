7ddbe65739d339085a4f2d2c5a63c6b2eccc2209
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-29 00:25:32 -0400

    Adding bid feature using lls

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,7 +1,38 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
+import { bidHistoryKey } from '$services/keys';
+import { client } from '$services/redis';
+import { DateTime } from 'luxon';
 
-export const createBid = async (attrs: CreateBidAttrs) => {};
+export const createBid = async (attrs: CreateBidAttrs) => {
+	const serialized = serializeHistory(
+			attrs.amount,
+			attrs.createdAt.toMillis(),
+		);
+
+		return client.rPush(bidHistoryKey(attrs.itemId), serialized);
+};
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
-	return [];
+	const startIndex = -1 * offset - count;
+	const endIndex = -1 - offset;
+
+	const range = await client.lRange(bidHistoryKey(itemId),
+		startIndex,
+		endIndex
+	);
+
+	return range.map(bid => deserializeHistory(bid));
+};
+
+const serializeHistory = (amount: number, createdAt: number) => {
+	return `${amount}:${createdAt}`;
+};
+
+const deserializeHistory = (stored: string) => {
+	const [amount, createdAt] = stored.split(":");
+
+	return {
+		amount: parseFloat(amount),
+		createdAt: DateTime.fromMillis(parseInt(createdAt)),
+	}
 };
--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,14 +1,18 @@
 import { client } from "$services/redis";
 import { itemsKey, itemsByViewsKey } from "$services/keys";
+import { deserialize } from "./deserialize";
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-  const results = await client.sort(
+  let results: any = await client.sort(
     itemsByViewsKey(),
     {
       GET: [
         '#',
         `${itemsKey("*")}->name`,
-        `${itemsKey("*")}->views`
+        `${itemsKey("*")}->views`,
+        `${itemsKey("*")}->endingAt`,
+        `${itemsKey("*")}->imageUrl`,
+        `${itemsKey("*")}->price`
       ],
       BY: 'score',
       DIRECTION: order,
@@ -19,5 +23,13 @@ export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, c
     }
   );
 
-  console.log(results);
+  const items = [];
+  while (results.length) {
+    const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+    const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+    items.push(item);
+    results = rest;
+  }
+
+  return items;
 };
