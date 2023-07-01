34ea59300201c4ebe41b6de9e1b1484979dd4b57
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-30 22:59:32 -0400

    Adding lua script to client

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,12 +1,17 @@
-import { createClient } from 'redis';
+import { createClient, defineScript } from 'redis';
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts: {
+		addOneAndStore: defineScript({})
+	}
 });
+
+client.addOneAndStore()
 
 client.on('error', (err) => console.error(err));
 client.connect();
