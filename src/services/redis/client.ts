2d1118f51b47879bfd99b44d3e65120e95ff375d
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-30 23:30:14 -0400

    Updating keys with lua scripts

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,4 +1,5 @@
 import { createClient, defineScript } from 'redis';
+import { incrementView } from '../queries/views';
 
 const client = createClient({
 	socket: {
@@ -20,6 +21,24 @@ const client = createClient({
 			transformReply(reply: any) {
 				return reply
 			}
+		}),
+		incrementView: defineScript({
+			NUMBER_OF_KEYS: 3,
+			SCRIPT: `
+				local itemsViewsKey = KEYS[1]
+				local itemsKey = KEYS[2]
+				local itemsByViewsKey = KEYS[3]
+
+				local itemId = ARGV[1]
+				local userId = ARGV[2]
+
+				local inserted = redis.call('PFADD', itemsViewsKey, userId)
+
+				if inserted == 1 then
+					redis.call('HINCRBY', itemsKey, 'views', 1)
+					redis.call('ZINCRBY', itemsByViewsKey, 1, itemId)
+				end
+			`
 		})
 	}
 });
