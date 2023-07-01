1fe1fd916b081ae338c1a087d80ce3a839ed4ae3
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-30 23:09:13 -0400

    Adding lua script to client

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -7,11 +7,28 @@ const client = createClient({
 	},
 	password: process.env.REDIS_PW,
 	scripts: {
-		addOneAndStore: defineScript({})
+		addOneAndStore: defineScript({
+			NUMBER_OF_KEYS: 1,
+			SCRIPT: `
+				return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))
+			`,
+			transformArguments(key: string, value: number) {
+				return [key, value.toString()]
+			},
+			transformReply(reply: any) {
+				return reply
+			}
+		})
 	}
 });
 
-client.addOneAndStore()
+// client.addOneAndStore();
+client.on('connect', async () => {
+	await client.addOneAndStore('books:count', 10);
+
+	const result = await client.get('books:count');
+	console.log(result);
+})
 
 client.on('error', (err) => console.error(err));
 client.connect();
