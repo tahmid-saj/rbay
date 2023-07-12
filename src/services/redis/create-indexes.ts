9ad5ff45c15251e8f810caa51165e6835ad7b08e
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-12 17:19:52 -0400

    Updating index with sorting

--- src/services/redis/create-indexes.ts
+++ src/services/redis/create-indexes.ts
@@ -15,12 +15,38 @@ export const createIndexes = async () => {
     itemsIndexKey(),
     {
       name: {
-        type: SchemaFieldTypes.TEXT
+        type: SchemaFieldTypes.TEXT,
+        SORTABLE: true
       },
       description: {
-        type: SchemaFieldTypes.TEXT
+        type: SchemaFieldTypes.TEXT,
+        SORTABLE: false
+      },
+      ownerId: {
+        type: SchemaFieldTypes.TAG,
+        sortable: false
+      },
+      endingAt: {
+        type: SchemaFieldTypes.NUMERIC,
+        sortable: true
+      },
+      bids: {
+        type: SchemaFieldTypes.NUMERIC,
+        sortable: true
+      },
+      views: {
+        type: SchemaFieldTypes.NUMERIC,
+        sortable: true
+      },
+      price: {
+        type: SchemaFieldTypes.NUMERIC,
+        sortable: true
+      },
+      likes: {
+        type: SchemaFieldTypes.NUMERIC,
+        sortable: true
       }
-    },
+    } as any,
     {
       ON: 'HASH',
       PREFIX: itemsKey('')
